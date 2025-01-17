b03dd30311045d45e5e62aa4636ec9d23ca59a4b
unknown <raohamza936@gmail.com>
2025-01-17 13:00:58 +0500

    sidemenu close outside clicking

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import logo from "../assets/img/logo.png";
 import Sidebar from "./sidebar";
 import LoginBtn from "./ui/login-btn";
 import SignUpBtn from "./ui/signup-btn";
 import { FaBars } from "react-icons/fa6";
+import { useOnClickOutside } from "../hooks/use-on-click-outside";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
+  const menuRef = useRef(null);
+  const [toggleMenu, setToggleMenu] = useState(false)
+  useOnClickOutside(menuRef, () => setToggleMenu(false));
   return (
     <div className="">
       <div className="flex items-center justify-between bg-[#f9fbff8f] fixed top-0 w-full px-6 h-16  backdrop-blur-sm z-30 lg:p-3 lg:px-14 ">
@@ -39,11 +42,13 @@ const Navbar = () => {
         </div>
         <FaBars
           className="md:hidden lg:hidden xl:hidden"
-          onClick={() => setNav(!nav)}
+          onClick={() => setToggleMenu(true)}
         />
       </div>
-      {nav ? (
-        <div className="fixed w-full z-40">
+      {toggleMenu ? (
+        <div 
+        ref={menuRef}
+        className="fixed w-full z-40">
           <Sidebar />
         </div>
       ) : null}
